98f1b293d3824b4ad65bcc452cc1d16d6cba75db
Animesh Singh <animeshritika123@gmail.com>
2023-12-20 18:10:42 +0530

    added project banner
    project banner

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Contact, Experince, Header } from "../containers";
+import ProjectBanner from "../components/projectBanner/ProjectBanner";
 
 const Home = () => {
   return (
     <>
       <Header />
       <Experince />
+      <ProjectBanner />
       <Contact />
     </>
   );
